de3307230f182758cf8ab713e4b050d8ce5a959d
Edso <edso1999@gmail.com>
2020-09-15 21:43:11 -0600

    Auto stash before merge of "dev" and "origin/dev"

--- src/web/routes/main.routes.js
+++ src/web/routes/main.routes.js
@@ -15,6 +15,10 @@ router.get('/registrarse',(req,res)=>{
     res.render('client/registrarse');
 });
 
+router.get('/perfil',(req,res)=>{
+    res.render('client/perfil');
+});
+
 router.post('/profesores',(req,res)=>{
     var script = con.query('select * from t_profesor',
     (err,rows,fields)=>{
